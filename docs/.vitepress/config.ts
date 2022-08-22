a46485bdf99225cb76f21dd14a8d91b0f552d222
CHOYSEN <choysen@qq.com>
2022-08-22 23:09:05 +0800

    chore: add basic workflow

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,18 +1,18 @@
-import { defineConfig } from "vitepress";
+import { defineConfig } from 'vitepress';
 
 export default defineConfig({
-  title: "OOOOOOA",
+  title: 'OOOOOOA',
   themeConfig: {
     sidebar: [
       // React 专题
       {
-        text: "React 专题",
-        items: [{ text: "Hooks 分享", link: "/react/hooks/index" }],
+        text: 'React 专题',
+        items: [{ text: 'Hooks 分享', link: '/react/hooks/index' }],
       },
       // React Native 专题
       {
-        text: "React Native 专题",
-        items: [{ text: "源码分享", link: "/react-native/source/index" }],
+        text: 'React Native 专题',
+        items: [{ text: '源码分享', link: '/react-native/source/index' }],
       },
     ],
   },
